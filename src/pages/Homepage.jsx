5f99941cc9350f082993c65e56bdfe065259cc61
dhanianwr <dhanyaries@gmail.com>
2024-06-07 13:49:10 +0700

    adding tailwinds, halaman home

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <div className='h-screen flex w-screen justify-center items-center'>
+            <h1 className='font-bold'>Halaman Home</h1>
+    </div>
+  )
+}
